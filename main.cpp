5b3500429ce077606ab71e6578ab50c4473b55a6
chidubem6 <chidubemokafor1@outlook.com>
2026-04-19 14:52:16 +0100

    Add .gitignore and enhance main.cpp with user login and account management features

--- main.cpp
+++ main.cpp
@@ -39,14 +39,40 @@ int main() {
             std::cin >> pin;
 
             Account* user = bank.logIn(accNum, pin);
-
+            
             if (user != nullptr) {
                 std::cout << "Login successful. Welcome, " << user->getName() << "\n";
+
+                int loginChoice;
+                do {
+                    std::cout << "1. Deposit\n";
+                    std::cout << "2. Withdraw\n";
+                    std::cout << "3. Check Balance\n";
+                    std::cout << "4. Logout\n";
+                    std::cin >> loginChoice;
+
+                    if (loginChoice == 1) {
+                        double amount;
+                        std::cout << "Input deposit amount: \n" ;
+                        std::cin >> amount;
+                        user->deposit(amount);
+                    }
+                    else if (loginChoice == 2) {
+                        double amount;
+                        std::cout << "Input withdraw amount: \n" ;
+                        std::cin >> amount;
+                        user->withdraw(amount);
+                    }
+                    else if (loginChoice == 3) {
+                        std::cout << user->getBalance() << "\n";
+                    }
+
+                } while (loginChoice != 4);
+
             } else {
                 std::cout << "Invalid login.\n";
-            }
+            } 
         }
-
     } while (choice != 3);
 
     return 0;
